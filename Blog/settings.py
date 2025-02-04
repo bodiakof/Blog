614a7d3c1b945541d9f896f1a073ff2709087961
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-02-04 15:45:43 +0200

    Pre GPT code

--- Blog/settings.py
+++ Blog/settings.py
@@ -31,11 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "blog_app"
 ]
 
 MIDDLEWARE = [
@@ -115,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "blog_app/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from .forms import CommentForm
+from .models import Post
+
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(
+        request,
+        "blog/post/list.html",
+        {"posts": posts},
+    )
+
+def post_detail(
+        request: HttpRequest,
+        year: int,
+        month: int,
+        day: int,
+        post: str
+) -> HttpResponse:
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
+    return render(
+        request,
+        "blog/post/detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+         }
+    )
+
+@require_POST
+def post_comment(request: HttpRequest, post_id: int) -> HttpResponse:
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        "blog/post/comment.html",
+        {
+            "post": post,
+            "form": form,
+            "comment": comment
+        },
+    )
