b80e5cee62598904eefc73e234a0e0f27de1de86
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-02-16 11:50:49 +0200

    Used os.environ for secret key

--- Blog/settings/base.py
+++ Blog/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.conf.global_settings import STATIC_ROOT
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ("django-insecure-m%(3b$+owc(6b10^c0lxs8-"
-              "*yz9(ai4iy0$!5gtuw8x5en9e9c")
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # Application definition
 
